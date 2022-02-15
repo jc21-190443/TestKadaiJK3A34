047454a333f304a4b5e86dad122eba430f541ec1
龍之介 前川 <190443@jc-21.jp>
2022-02-15 12:33:53 +0900

    メソッド名を修正

--- kuriage.cs
+++ kuriage.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApp1
     public class kuriage
     {
 
-        public static int Kuriage10(int j)
+        public static int kuriage10(int j)
         {
             int s = j / 10;
             if (j % 10 != 0)
